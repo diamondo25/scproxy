e207fa6c68d4d96aaa8af230300c85767d27ba74
Erwin Oegema <blablaechthema@hotmail.com>
2020-09-16 21:24:15 +0200

    Add more stuff. not working remove lingering server

--- main.go
+++ main.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"crypto/tls"
+	"fmt"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
+	"io"
 	"io/ioutil"
 	"log"
+	"net"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"os"
+	"time"
 )
 
 func serveFile(path string) func(w http.ResponseWriter, r *http.Request) {
@@ -17,7 +21,7 @@ func serveFile(path string) func(w http.ResponseWriter, r *http.Request) {
 		f, err := ioutil.ReadFile(path)
 		if err != nil {
 			log.Print("Error: ", err)
-			http.Error(w, "error while getting desktop.html: " + err.Error(), http.StatusInternalServerError)
+			http.Error(w, "error while getting desktop.html: "+err.Error(), http.StatusInternalServerError)
 			return
 		}
 
@@ -25,11 +29,18 @@ func serveFile(path string) func(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-
 func main() {
 	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
 
 	r := mux.NewRouter()
+
+	makeDomainProxy := func(domain string, upstream string) *mux.Router {
+		router := r.Host(domain).Subrouter()
+		proxyURL, _ := url.Parse(upstream)
+		router.PathPrefix("/").Handler(httputil.NewSingleHostReverseProxy(proxyURL))
+
+		return router
+	}
 
 	version := "15042217"
 	ros := r.Host("prod.ros.rockstargames.com").Subrouter()
@@ -38,10 +49,7 @@ func main() {
 	ros.HandleFunc("/scui/v2/ext/scui/"+version+"/js/common/jquery-1.7.2.min.js", serveFile("jquery-1.7.2.min.js"))
 
 	// 192.81.241.100 == prod.ros.rockstargames.com
-	remote, err := url.Parse("http://192.81.241.100/scui/mtl/api/")
-	if err != nil {
-		log.Panic("unable to parse ros url wtf: ", err)
-	}
+	remote, _ := url.Parse("http://192.81.241.100/scui/mtl/api/")
 	proxy := httputil.NewSingleHostReverseProxy(remote)
 
 	ros.PathPrefix("/scui/v2/api/").Handler(http.StripPrefix("/scui/v2/api/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -56,21 +64,31 @@ func main() {
 		proxy.ServeHTTP(w, r)
 	})))
 
-	cloud := r.Host("prod.cloud.rockstargames.com").Subrouter()
+	global, _ := url.Parse("http://192.81.241.100/")
+	globalProxy := httputil.NewSingleHostReverseProxy(global)
+
+	ros.PathPrefix("/").Handler(globalProxy)
+
+	// prod.cloud.rockstargames.com.edgekey.net
+
+	cloud := makeDomainProxy("prod.cloud.rockstargames.com", "http://prod.cloud.rockstargames.com.edgekey.net/")
+	//cloud := r.Host("prod.cloud.rockstargames.com").Subrouter()
 
 	globalScui := "/global/ext/scui/"
 
-	cloud.PathPrefix(globalScui+version+"/").Handler(http.StripPrefix(globalScui+version+"/", http.FileServer(http.Dir("./UI/ext/scui/a/"))))
+	cloud.PathPrefix(globalScui + version + "/").Handler(http.StripPrefix(globalScui+version+"/", http.FileServer(http.Dir("./UI/ext/scui/a/"))))
 	cloud.PathPrefix(globalScui).Handler(http.StripPrefix(globalScui, http.FileServer(http.Dir("./UI/ext/scui/"))))
 
-
+	authProd := makeDomainProxy("auth-prod.ros.rockstargames.com", "https://192.81.241.100/")
+	_ = authProd
 
 	log.Print("Starting server...")
 	cfg := &tls.Config{}
 
-	for _, x := range []string {
+	for _, x := range []string{
 		"prod_ros_rockstargames_com",
 		"prod_cloud_rockstargames_com",
+		"ros_rockstargames_com",
 	} {
 		cert, err := tls.LoadX509KeyPair(
 			x+".crt",
@@ -83,14 +101,52 @@ func main() {
 		cfg.Certificates = append(cfg.Certificates, cert)
 	}
 
-	// keep adding remaining certs to cfg.Certificates
+	customLogger := func(protocol string) handlers.LogFormatter {
+		return func(writer io.Writer, params handlers.LogFormatterParams) {
 
-	// cfg.BuildNameToCertificate()
+			statusText := ""
+			if params.StatusCode != 200 {
+				statusText = "<<<<< !!!!"
+			}
+
+			fmt.Fprintln(writer,
+				params.TimeStamp.Format(time.RFC3339),
+				params.Request.Method,
+				protocol+"://"+params.Request.Host+params.Request.URL.String(),
+				params.Size,
+				params.StatusCode,
+				statusText,
+			)
+		}
+	}
+
+	go func() {
+		log.Fatal(http.ListenAndServe("127.0.0.1:80", handlers.CustomLoggingHandler(os.Stdout, r, customLogger("http"))))
+	}()
+
+	go func() {
+		// Lingering server
+		l, err := net.Listen("tcp", "127.0.0.1:443")
+		if err != nil {
+			panic(err)
+		}
+
+		for {
+			conn, err := l.Accept()
+			fmt.Println("Got a connection... but not going to do anything with it")
+			if err != nil {
+				panic(err)
+			}
+
+			_ = conn
+		}
+
+	}()
 
 
 	server := http.Server{
-		Addr:      "127.0.0.1:443",
-		Handler:   handlers.LoggingHandler(os.Stdout, r),
+		Addr:      "127.0.0.1:4433",
+		Handler:   handlers.CustomLoggingHandler(os.Stdout, r, customLogger("https")),
 		TLSConfig: cfg,
 	}
 
